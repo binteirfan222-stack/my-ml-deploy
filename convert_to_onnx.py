5a2c2e0bc9541d0298e9876e1ad7e454a76492ce
Sadia irfan <bint.e.irfan222@gmail.com>
2026-07-28 12:06:05 +0500

    fix skops trusted loading for new version

--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -1,8 +1,13 @@
 import skops.io as sio
 from skl2onnx import to_onnx
 import numpy as np
+import os
 
-pipe = sio.load("./Model/model_pipeline.skops", trusted=True)
+os.makedirs("./App", exist_ok=True)
+
+untrusted_types = sio.get_untrusted_types(file="./Model/model_pipeline.skops")
+pipe = sio.load("./Model/model_pipeline.skops", trusted=untrusted_types)
+
 sample_input = np.zeros((1, 3), dtype=np.float32)
 onnx_model = to_onnx(pipe, sample_input)
 
